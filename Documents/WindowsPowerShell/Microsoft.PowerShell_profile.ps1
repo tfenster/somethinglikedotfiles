b6a866868f7353d3784c81db72c1e9ba802de627
Tobias Fenster <tobias.fenster@cosmoconsult.com>
2020-05-17 09:30:18 +0200

    more consistent results

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,18 +1,19 @@
 function t {   
-    # Windows Theme
+    # Windows Theme and define mode to be consistent 
     $themePath = "Registry::HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize"
     $AppsUseLightTheme = Get-ItemProperty -Path $themePath -Name AppsUseLightTheme
-    $SystemUsesLightTheme = Get-ItemProperty -Path $themePath -Name SystemUsesLightTheme
-    Set-ItemProperty -Path $themePath -Name AppsUseLightTheme -Value (($AppsUseLightTheme.AppsUseLightTheme / 1 + 1) % 2) -Type DWord
-    Set-ItemProperty -Path $themePath -Name SystemUsesLightTheme -Value (($SystemUsesLightTheme.SystemUsesLightTheme / 1 + 1) % 2) -Type DWord
+    $goDark = ($AppsUseLightTheme.AppsUseLightTheme / 1) -eq 1
+    
+    Set-ItemProperty -Path $themePath -Name AppsUseLightTheme -Value ($(if ($goDark) { 0 } else { 1 })) -Type DWord
+    Set-ItemProperty -Path $themePath -Name SystemUsesLightTheme -Value ($(if ($goDark) { 0 } else { 1 })) -Type DWord
 
     # Terminal
     $settingsPath = "$env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
     $terminalLightTheme = "One Half Light"
     $terminalDarkTheme = "One Half Dark"
-    if (Select-String -Path $settingsPath -Pattern $terminalDarkTheme) {
-        ((Get-Content $settingsPath) -replace $terminalDarkTheme, $terminalLightTheme) | Set-Content $settingsPath
+    if ($goDark) {
+        ((Get-Content $settingsPath) -replace $terminalLightTheme, $terminalDarkTheme) | Set-Content $settingsPath
     } else {
-        ((Get-Content $settingsPath)  -replace $terminalLightTheme, $terminalDarkTheme) | Set-Content $settingsPath
+        ((Get-Content $settingsPath) -replace $terminalDarkTheme, $terminalLightTheme) | Set-Content $settingsPath
     }
 }
